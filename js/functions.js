15699c01f4446c0d9df3180c555322e623c73a83
Suninall <sanina-nsk@mail.ru>
2024-11-25 11:22:03 +0700

    добавляет вызов функций

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,14 @@
 function countLength (inputString, maxLen) {
   const amount = inputString.length;
   return amount <= maxLen;
-
 }
 
-countLength('проверяемая строка', 20);
-countLength('проверяемая строка', 18);
-countLength('проверяемая строка', 10);
+countLength("проверяемая строка", 10);
 
 function checkPalindrom(stringPalindrom){
   stringPalindrom = stringPalindrom.toLowerCase();
-  const reverseString = stringPalindrom.split('').reverse().join('');
+  const reverseString = stringPalindrom.split("").reverse().join("");
   return stringPalindrom === reverseString;
 }
 
-
-
+checkPalindrom("топот");
