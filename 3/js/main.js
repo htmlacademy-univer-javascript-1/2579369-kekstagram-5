982c827ccc8d307cff923a6d9b8d4127879ddd89
Keks <keks@htmlacademy.ru>
2024-12-02 18:48:56 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -35,12 +35,13 @@ const getRandomInteger = (a, b) => {
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
+
 const createComment = () => {
   const randomNameIndex = getRandomInteger(0, NAMES.length - 1);
   return {
     id:getRandomInteger(0,30),
     avatar:`img/avatar-${ getRandomInteger(1,6) }.svg`,
-    message: Array.from({length:getRandomInteger(1,2)}, () => MESSAGES[getRandomInteger(0, MESSAGES.length - 1)]),
+    message: Array.from({length:getRandomInteger(1,2)}, () => MESSAGES[getRandomInteger(0, MESSAGES.length - 1)]).join("\n"),
     name: NAMES[randomNameIndex]
   };
 };
