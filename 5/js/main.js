efa4fe99fdbafccaaf53bc4bd247616a4b2cc918
Keks <keks@htmlacademy.ru>
2024-12-08 01:58:47 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,3 @@
+import {createPost} from "./data.js";
+import {POST_COUNT} from "./constants.js";
+import "./rendering.js";
---
+++ 5/js/rendering.js
@@ -0,0 +1,22 @@
+import {createPost} from "./data.js";
+import {POST_COUNT} from "./constants.js";
+
+const posts = Array.from({length:POST_COUNT},createPost);
+
+const pictureTemplate = document.querySelector("#picture").content;
+const picturesContainer = document.querySelector(".pictures");
+
+const fragment = document.createDocumentFragment();
+
+posts.forEach((post)=>{
+  const postsElement = pictureTemplate.cloneNode(true);
+  const pictureImg = postsElement.querySelector(".picture__img");
+  pictureImg.src = post.url;
+  pictureImg.alt = post.description;
+  const photolikes = postsElement.querySelector(".picture__likes");
+  photolikes.textContent = post.likes;
+  const pictureComments = postsElement.querySelector(".picture__comments");
+  pictureComments.textContent = post.comments.length;
+  fragment.appendChild(postsElement);
+});
+picturesContainer.appendChild(fragment);
