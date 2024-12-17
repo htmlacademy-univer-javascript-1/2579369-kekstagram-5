a2baf7c4fe5a2322b30b5382e05df453e01199d9
Keks <keks@htmlacademy.ru>
2024-12-18 02:45:15 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/close-open-post.js
+++ 6/js/close-open-post.js
@@ -6,17 +6,19 @@ const bigPictureImg = bigPicture.querySelector(".big-picture__img img");
 const likesCount = bigPicture.querySelector(".likes-count");
 const commentsCount = bigPicture.querySelector(".comments-count");
 const socialCaption = bigPicture.querySelector(".social__caption");
-
-
+const body = document.querySelector("body");
+const socialComments = bigPicture.querySelector(".social__comments");
 function closePosts(){
   buttonClosePhoto.addEventListener("click", () => {
     bigPicture.classList.add("hidden");
+    body.classList.remove("modal-open");
   });
 }
 document.addEventListener("keydown", (evt) => {
   if (evt.key === "Escape") {
     evt.preventDefault();
     bigPicture.classList.add("hidden");
+    body.classList.remove("modal-open");
   }
 });
 
@@ -26,16 +28,22 @@ function defineBigPicture(post){
   likesCount.textContent = likes;
   commentsCount.textContent = comments.length;
   socialCaption.textContent = description;
+  socialComments.textContent = "";
   comments.forEach((comment)=>{
-    if (comment){
-      const avatar = comment.querySelector('.social__picture');
-      const text = comment.querySelector('.social__text');
-      avatar.src = comments.avatar;
-      avatar.alt = comments.name;
-      text.textContent = comment.message;
-    }
+    const commentItem = document.createElement("li");
+    commentItem.classList.add("social__comment");
+    const commentImg = document.createElement("img");
+    commentImg.classList.add("social__picture");
+    const commentText = document.createElement("p");
+    commentText.classList.add("social__text");
+    commentImg.src = comment.avatar;
+    commentImg.alt = comment.name;
+    commentItem.textContent = comment.message;
+    commentItem.appendChild(commentImg);
+    commentItem.appendChild(commentText);
+    socialComments.appendChild(commentItem);
   });
 }
 closePosts();
 
-export{defineBigPicture};
+export{defineBigPicture, bigPicture,body};
