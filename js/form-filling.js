199e614dea91f235b229ca80206345db4f68b489
Suninall <sanina-nsk@mail.ru>
2024-12-24 20:30:57 +0700

    добавление шаблонов

--- js/form-filling.js
+++ js/form-filling.js
@@ -3,11 +3,13 @@ const body = document.body;
 const imgOverlay = document.querySelector(".img-upload__overlay");
 const closeImg = document.querySelector(".img-upload__cancel");
 const postForm = document.querySelector(".img-upload__form");
+const formButton = document.querySelector(".img-upload__submit");
 const pristine = new Pristine(postForm, {
   classTo: "img-upload__text",
   errorTextParent: "img-upload__text",
 });
 const regexp = /^#[^\s#]+$/i;
+
 imgInput.addEventListener("change", (evt) => {
   evt.preventDefault();
   imgOverlay.classList.remove("hidden");
@@ -23,9 +25,15 @@ imgInput.addEventListener("change", (evt) => {
 
 function validateHastags (value) {
   const hashtags = value.trim().split(" ");
+  if (value.trim() === "") {
+    return true;
+  }
   return hashtags.every((hashtag) => regexp.test(hashtag));
 }
 function validateComment (value) {
+  if (value.trim() === "") {
+    return true;
+  }
   return value.length <= 140;
 }
 
@@ -54,18 +62,75 @@ pristine.addValidator(
   validateComment,
   "комментарий не может содрежать больше 140 символов"
 );
+const successTemplate = document.querySelector("#success").content;
+const successElement = successTemplate.cloneNode(true).querySelector(".success");
+const successButton = successElement.querySelector(".success__button");
+function openSucsess (){
+  document.body.append(successElement);
+  successButton.addEventListener("click", closeSuccess);
+}
+function closeSuccess(){
+  successButton.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    successElement.remove();
+  });
+  document.addEventListener("keydown", (evt) => {
+    if (evt.key === "Escape") {
+      evt.preventDefault();
+      successElement.remove();
+    }
+  });
+  document.addEventListener("click", (evt) => {
+    if (successElement && !successElement.contains(evt.target)) {
+      successElement.remove();
+    }
+  });
+}
 
+const errorTemplate = document.querySelector("#error").content;
+const errorElement = errorTemplate.cloneNode(true).querySelector(".error");
+const errorButton = errorElement.querySelector(".error__button");
+function openError (){
+  document.body.append(errorElement);
+  errorButton.addEventListener("click", closeError);
+}
+function closeError(){
+  errorButton.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    errorElement.remove();
+  });
+  document.addEventListener("keydown", (evt) => {
+    if (evt.key === "Escape") {
+      evt.preventDefault();
+      errorElement.remove();
+    }
+  });
+}
+
+postForm.addEventListener("submit",(evt) => {
+  evt.preventDefault();
+  formButton.disabled = true;
+  if(pristine.validate()){
+    openSucsess();
+    formButton.disabled = false;
+  }else {
+    openError();
+    formButton.disabled = false;
+  }
+
+});
 function closeForm(){
   closeImg.addEventListener("click", () => {
     imgOverlay.classList.add("hidden");
     body.classList.remove("modal-open");
   });
+  document.addEventListener("keydown", (evt) => {
+    if (evt.key === "Escape") {
+      evt.preventDefault();
+      imgOverlay.classList.add("hidden");
+      body.classList.remove("modal-open");
+    }
+  });
 }
-document.addEventListener("keydown", (evt) => {
-  if (evt.key === "Escape") {
-    evt.preventDefault();
-    imgOverlay.classList.add("hidden");
-    body.classList.remove("modal-open");
-  }
-});
 closeForm();
+
