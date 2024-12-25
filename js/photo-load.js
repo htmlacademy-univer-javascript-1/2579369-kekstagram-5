587f320fa2e76d3b54f8cc9cf6a13af178db9842
Suninall <sanina-nsk@mail.ru>
2024-12-25 14:49:22 +0700

    выносит константы в модуль с константами

--- js/photo-load.js
+++ js/photo-load.js
@@ -1,8 +1,4 @@
-const FILE_TYPES = [
-  "jpg",
-  "jpeg",
-  "png"
-];
+import {FILE_TYPES} from "./constants.js";
 function loadPhoto(){
   const imgInput = document.querySelector(".img-upload__input");
   const preview = document.querySelector(".img-upload__preview img");
@@ -11,7 +7,6 @@ function loadPhoto(){
     const file = imgInput.files[0];
     const fileName = file.name.toLowerCase();
     const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
-    console.log(matches);
     if(matches){
       preview.src = URL.createObjectURL(file);
     }
